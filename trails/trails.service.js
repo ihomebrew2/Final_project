8360d83d9d8a65c3f23a7a38a22db295eb64a345
ihomebrew2 <ihomebrew2@yahoo.com>
2018-03-17 16:56:50 -0400

    Details associated with each trail now render on the page.

--- trails/trails.service.js
+++ trails/trails.service.js
@@ -5,13 +5,13 @@ angular.module("trailApp").service("TrailService", ['$http', function ($http) {
 			params: {
 				lat: 42.96,
 				lon: -85.66,
-				limit: 50,
-				radius: 10
+				limit: 3,
+				radius: 25
 			},
 			headers: { 'X-Mashape-Key': 'dVYNFyBiYUmshR4Da1wHTOQTXsxcp1b5we0jsnYtmQR6XqqfID' }
 		}).then(
 			response => {
-				console.log(response.data.places[0]);
+				console.log(response.data.places);
 
 				return response.data.places;
 			}
